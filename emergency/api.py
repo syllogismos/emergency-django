30797608a846b0f70e43dc01c188d9052ba1e0a9
Anil Karaka <anilkaraka@live.in>
2014-04-16 13:05:52 +0000

    update api

--- emergency/api.py
+++ emergency/api.py
@@ -1,9 +1,20 @@
 
 from tastypie.resources import ModelResource
+from tastypie import fields
+
 from emergency.models import Hospital
+
+from django.contrib.auth.models import User
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+	resource_name = 'hospital_admin'
 
 
 class HospitalResource(ModelResource):
+    hospital_admin = fields.ForeignKey(UserResource, 'hospital_admin')
     class Meta:
 	queryset = Hospital.objects.all()
 	resource_name = 'hospital'
--- erscout/urls.py
+++ erscout/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django_otp.forms import OTPAuthenticationForm
 from django.views.generic import RedirectView
-from emergency.api import HospitalResource
 
+from emergency.api import HospitalResource
+from emergency.api import UserResource
+
+from tastypie.api import Api
 
 from django.contrib import admin
 admin.autodiscover()
 
-hospital_resource = HospitalResource()
-
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(HospitalResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -18,5 +22,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('two_factor.urls', 'two_factor')),
     url(r'^accounts/logout/$', RedirectView.as_view(url='/admin/logout/')),
-    url(r'^api/', include(hospital_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )

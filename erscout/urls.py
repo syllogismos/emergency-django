db7bd44b29b5da7b9132f284b318271a7962e70e
Anil Karaka <anilkaraka@live.in>
2014-04-16 12:38:32 +0000

    added api urls in main url conf

--- erscout/urls.py
+++ erscout/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django_otp.forms import OTPAuthenticationForm
 from django.views.generic import RedirectView
+from emergency.api import HospitalResource
+
 
 from django.contrib import admin
 admin.autodiscover()
+
+hospital_resource = HospitalResource()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,4 +18,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('two_factor.urls', 'two_factor')),
     url(r'^accounts/logout/$', RedirectView.as_view(url='/admin/logout/')),
+    url(r'^api/', include(hospital_resource.urls)),
 )

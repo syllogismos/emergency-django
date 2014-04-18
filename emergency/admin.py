7fbae3e7641c36319ea5b8af1306306d99e03224
Anil Karaka <anilkaraka@live.in>
2014-04-18 09:14:26 +0000

    make the hospitals only available to their own hospital admins and the superuser., really elegant solution.

--- emergency/admin.py
+++ emergency/admin.py
@@ -4,6 +4,11 @@ from emergency.models import Hospital, Check_in
 # Register your models here.
 
 class HospitalAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+	qs = super(HospitalAdmin, self).queryset(request)
+	if request.user.is_superuser:
+	    return qs
+	return qs.filter(hospital_admin = request.user)
     list_display = ('provider_id', 'hospital_admin', 'hospital_name', 'address', 'city', 'state', 'zip_code', 'phone_number', 'ed2', 'ed1', 'left_before_being_seen', 'op18', 'door_to_diagnostic_eval', 'median_time_to_pain_med',)
     search_fields = ['hospital_name']
 

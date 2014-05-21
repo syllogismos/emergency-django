bd94f76c1b61ddec39d815f2a684a4f25c24b7e4
jrslyrics <jrslyrics@gmail.com>
2014-05-21 21:12:20 +0000

    newlanding page

--- emergency/views.py
+++ emergency/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'landing.html',) 
+
+def newtemplate(request):
+  return render(request, 'newtemplate.html',)
